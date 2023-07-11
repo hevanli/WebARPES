701bc317805a4c5cf883209aedd97001be88466e
hevanli <evanlicubs@gmail.com>
2023-07-11 14:55:42 -0700

    added params route/page

--- app/frontend/routes.py
+++ app/frontend/routes.py
@@ -5,3 +5,7 @@ from app.frontend import frontendbp
 @frontendbp.route('/index')
 def index():
     return render_template('index.html', title='Home')
+
+@frontendbp.route('/setparams')
+def setparams():
+    return render_template('setparams.html', title='Set Parameters')
